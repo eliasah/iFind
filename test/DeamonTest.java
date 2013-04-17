0d6f500322b0963f3ecd7ba0521b45890700c018
Abou Haydar Elias <abouhaydar.elias@gmail.com>
2013-04-17 14:54:32 +0200

    Plein de truc

---
+++ test/DeamonTest.java
@@ -0,0 +1,14 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DeamonTest {
+
+	@Test
+	public void testDaemon() {
+		fail("Not yet implemented");
+	}
+
+}
