61a9a842c6bfd5aef83f701d4ad571ba61713fd2
ahl <ahl@openroam-prg-sg-1-173-76.net.univ-paris-diderot.fr>
2013-05-15 11:53:21 +0200

    SaxParser a remplir pour notre usage, debut de la construction de l'xml search

---
+++ src/MoteurDeRecherche/Test.java
@@ -0,0 +1,12 @@
+package MoteurDeRecherche;
+
+import java.text.SimpleDateFormat;
+
+public class Test {
+
+
+	public static void main(String[] args) {
+		
+	}
+
+}
---
+++ src/MoteurDeRecherche/TimeSlot.java
@@ -0,0 +1,20 @@
+package MoteurDeRecherche;
+import java.text.SimpleDateFormat;
+
+public class TimeSlot {
+	int dayB;
+	int monthB;
+	int yearB;
+	int dayE;
+	int monthE;
+	int yearE;
+	
+	public TimeSlot(int db, int mb, int yb, int de, int me, int ye){
+		dayB= db;
+		monthB = mb;
+		yearB = yb;
+		dayE = de;
+		monthE = me;
+		yearE = ye;
+	}
+}
