2227cecdb6e03119f83e788ebc290b6094057c40
Abou Haydar Elias <abouhaydar.elias@gmail.com>
2013-05-20 16:53:43 +0200

    SimpleSAXParser + SAXContentHandler

--- src/engine/search/Search.java
+++ src/engine/search/Search.java
@@ -1,6 +1,6 @@
 package engine.search;
 
-/** Objet qui represente un XML Search comme d�finie dans la DTD
+/** Objet qui represente un XML Search comme definie dans la DTD
  *  permet une manupulation facile contrairement a un XML
  * 
  * @author Ahl Mikael - Univ. Paris Denis Diderot
@@ -8,15 +8,19 @@ package engine.search;
  */
 public class Search {
 	
+	private boolean content;
+	private String extension;
 	private int id;
-	private boolean word;
-	private String content;
 	private String pathdir;
 	private String perm;
-	private String extension;
 	private TimeSlot timeSlot;
+	private String word;
 	
-	public Search(int i, boolean w, String cont, String path, String permission, String ext, TimeSlot t){
+	public Search(int i) {
+		id = i;
+	}
+	
+	public Search(int i, String w, boolean cont, String path, String permission, String ext, TimeSlot t){
 		id = i;
 		word = w;
 		content = cont;
@@ -25,11 +29,9 @@ public class Search {
 		extension = ext;
 		timeSlot = t;
 	}
-	
-	public Search(int i) {
-		id = i;
-	}
 
+	
+	
 	public String ConvertToXml(){
 		String file;
 		file = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
@@ -61,5 +63,38 @@ public class Search {
 		}
 		file += "</SEARCH>\n\0";
 		return file;
+	}
+
+	public boolean isContent() {
+		return content;
+	}
+
+	public String getExtension() {
+		return extension;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public String getPathdir() {
+		return pathdir;
+	}
+
+	public String getPerm() {
+		return perm;
+	}
+
+	public TimeSlot getTimeSlot() {
+		return timeSlot;
+	}
+
+	public boolean isWord() {
+		return word;
+	}
+
+	@Override
+	public String toString() {
+		return "Search#" + id + "#" + word + "#" + content + "#" + pathdir + "#" + perm + "#"+ extension + "#" + timeSlot;
 	}
 }
---
+++ src/engine/search/SimpleSaxParser.java
@@ -0,0 +1,43 @@
+package engine.search;
+
+import java.io.IOException;
+
+import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
+import org.xml.sax.helpers.XMLReaderFactory;
+
+/**
+ * Cette classe est livree telle quelle.
+ * @author smeric
+ * @version 1.0
+ */
+public class SimpleSaxParser {
+
+    //Simple parseur qui utilise le SimpleContentHandler
+	//Tout se fait dans le contentHandler, ceci ne sert qu'a le lancer
+        public SimpleSaxParser(String uri) throws SAXException, IOException {
+                        XMLReader saxReader = XMLReaderFactory.createXMLReader("org.apache.xerces.parsers.SAXParser");
+                        saxReader.setContentHandler(new SimpleContentHandler());
+                        saxReader.parse(uri);
+        }
+
+        public static void main(String[] args) {
+                if (0 == args.length || 2 < args.length) {
+                        System.out.println("Usage : SimpleSaxParser uri [parserClassName]");
+                        System.exit(1);
+                }
+
+                String uri = args[0];
+
+                String parserName = null;
+                if (2 == args.length) {
+                        parserName = args[1];
+                }
+
+                try {
+                        SimpleSaxParser parser = new SimpleSaxParser(uri);
+                } catch (Throwable t) {
+                        t.printStackTrace();
+                }
+        }
+}
