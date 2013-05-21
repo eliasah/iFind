80f998f3c01fbd558ee86f6f4c9bc219f9dd7fb7
ahl <ahl@openroam-prg-gm-1-129-223.net.univ-paris-diderot.fr>
2013-05-21 20:29:27 +0200

    changement le searchDBserveur en thread

--- src/engine/search/SearchDBServer.java
+++ src/engine/search/SearchDBServer.java
@@ -9,7 +9,7 @@ import java.net.Socket;
  * @author Ahl Mikael
  *
  */
-public class SearchDBServer {
+public class SearchDBServer extends Thread {
 
 	private int[] ports;
 	
@@ -17,16 +17,11 @@ public class SearchDBServer {
 		ports=p;
 	}
 	
-	public void start(){
+	public void run(){
+		int [] ports = {30000,30001,30002};
+		SearchDBServer server = new SearchDBServer(ports);
 		for (int i = 0; i < ports.length; i++) {
 			new DBServerConnectionListener(ports[i]).run();
 		}
 	}
-	
-	public static void main(String[] args) {
-		int [] ports = {30000,30001,30002};
-		SearchDBServer server = new SearchDBServer(ports);
-		server.start();
-	}
-
 }
